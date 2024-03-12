7c3a10f2f757b14bf25aea99933207d6de94beaa
E3Ha <emma.harrison@talawanda.org>
2024-03-12 14:02:21 -0400

    2:02 3/12/2024

--- src/NoteAssignment.java
+++ src/NoteAssignment.java
@@ -7,12 +7,89 @@ public class NoteAssignment {
 
         Scanner scan = new Scanner(System.in);
         String trash = "";
-        String move = "";
+        String move1 = "";
+        String move2 = "";
+        boolean done = false;
 
-        move = RPSchecker(scan, move);
-        System.out.println("Your Move: " + move);
+        do {
+            System.out.println("Player1 Move: ");
+            move1 = RPSchecker(scan, move1);
+
+            System.out.println("Player2 Move: ");
+            move2 = RPSchecker(scan, move2);
 
 
+            switch (move1) {
+                case "r":
+                    switch (move2) {
+                        case "r":
+                            System.out.println("Tie");
+                            break;
+                        case "p":
+                            System.out.println("Player2 Wins");
+                            break;
+                        case "s":
+                            System.out.println("Player1 Wins");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "p":
+                    switch (move2) {
+                        case "r":
+                            System.out.println("Player1 Wins");
+                            break;
+                        case "p":
+                            System.out.println("Tie");
+                            break;
+                        case "s":
+                            System.out.println("Player2 Wins");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "s":
+                    switch (move2) {
+                        case "r":
+                            System.out.println("Player2 Wins");
+                            break;
+                        case "p":
+                            System.out.println("Player1 Wins");
+                            break;
+                        case "s":
+                            System.out.println("Tie");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+            }
+
+                System.out.println("");
+                System.out.println("Continue? (Y,N)");
+
+                if (scan.hasNextDouble()) {
+                    System.out.println("Incorrect Input");
+                    trash = scan.nextLine();
+                } else {
+                    trash = scan.nextLine();
+                    trash = trash.toLowerCase();
+                    switch (trash) {
+                        case "y":
+                            break;
+                        case "n":
+                            done = true;
+
+                            break;
+                        default:
+                            System.out.println("Incorrect Input");
+                            break;
+                    }
+                }
+        }
+        while (done == false);
 
 
 
@@ -26,7 +103,7 @@ public class NoteAssignment {
 
         do {
             //Ask for move
-            System.out.println("Move: ");
+
             //Check for incorrect input
             if (scan.hasNextDouble()) {
                 System.out.println("Incorrect Input ");
